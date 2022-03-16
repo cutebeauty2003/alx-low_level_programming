638fb354a6898531fba57cf4f8ed22a3d8352b14
Aaliyah Salako <saaliyah1207@gmail.com>
2022-03-16 01:23:56 -0700

    Hoping

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - write a program that prints putchar
+ *
+ * Description: Print -putchar and a new line
+ *
+ * Return: 0 (Successful)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Another project i am understanding
