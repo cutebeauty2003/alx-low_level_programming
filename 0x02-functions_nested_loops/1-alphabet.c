dbca9d7b58bae7e7b96077018511f49e3058787c
Aaliyah Salako <saaliyah1207@gmail.com>
2022-03-16 02:19:24 -0700

    Hiiii

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print in lower case
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char i = 'a';
+
+	while (i <= 'z')
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
