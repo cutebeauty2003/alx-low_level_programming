ac4e951b66ffea08386f3933bca52811302427b4
Aaliyah Salako <saaliyah1207@gmail.com>
2022-03-16 13:57:12 -0700

    Grrr

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ * @i: first integer
+ * @d: second integer
+ *
+ * Return: 0 (Successful)
+ */
+
+int add(int i, int d)
+{
+	return (a + b);
+}
