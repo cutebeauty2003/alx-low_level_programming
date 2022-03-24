e9eb5fd7bf1f12720220d5661ba20f128e0a11b7
Aaliyah Salako <saaliyah1207@gmail.com>
2022-03-24 13:10:02 -0700

    Frustrated

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strcat - a function that concatenates two strings
+ * @dest: destination pointer to a char
+ * @src: source pointer to a char
+ * Return: return value of dest
+ */
+
+char *_strcat(char *dest, char *src);
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Hello pinters, arrays, and strings
